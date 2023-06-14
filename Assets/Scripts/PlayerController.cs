cd85216c2be2b98856aed75f0c3123cfdf0a2075
Nicolas.dev <nicolasrhila@gmail.com>
2023-06-13 19:41:09 -0500

    feat! improve animator with blend trees for player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 4.0f;
+    private bool walking = false;
+    private Vector2 lastMovement = Vector2.zero;
     private const string horizontal = "Horizontal";
     private const string vertical = "Vertical";
+    private const string lastHorizontal = "LastHorizontal";
+    private const string lastVertical = "LastVertical";
+    private const string walkingState = "Walking";
     private Animator animator;
 
     // Start is called before the first frame update
@@ -19,14 +24,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        walking = false;
+
         // space = velocity * time;
         if(Mathf.Abs(Input.GetAxisRaw(horizontal)) > 0.5f) 
         {
             this.transform.Translate(new Vector3(Input.GetAxisRaw(horizontal) * speed * Time.deltaTime, 0, 0));
+            walking = true;
+            // Se actualiza el Vector2 dependiendo de la entrada del Input
+            lastMovement = new Vector2(Input.GetAxisRaw(horizontal), 0);
         }
         if(Mathf.Abs(Input.GetAxisRaw(vertical)) > 0.5f) 
         {
             this.transform.Translate(new Vector3(0, Input.GetAxisRaw(vertical) * speed * Time.deltaTime, 0));
+            walking = true;
+            lastMovement = new Vector2(0, Input.GetAxisRaw(vertical));
         }
 
         // Se asigna en Update el cambio de estado dependiendo del Input
@@ -36,5 +48,15 @@ public class PlayerController : MonoBehaviour
 
         // Desde el animator desactivamos el Has Exit Time en cada transicion
         // para evitar que tenga que completarse para cambiar de estado
+
+        // Tambien es recomendable desactivar el Fixed Duration en settings del nuevo estado
+        // para que la transcicion sea lo mas rapida posible
+
+        // En el Animator lo mas optimo es usar Blend Trees (Arboles de mezla)
+        // ------
+        // Luego se notifican los cambios al Animator para estar sincronizado con los movimientos del controlador
+        animator.SetBool(walkingState, walking);
+        animator.SetFloat(lastHorizontal, lastMovement.x);
+        animator.SetFloat(lastVertical, lastMovement.y);
     }
 }
